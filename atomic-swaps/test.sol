2b37b03f96f5a632803a88e7879f5662727e31c2
Michael Colburn <mgcolburn@users.noreply.github.com>
2018-05-22 15:18:35 -0400

    Create test.sol

---
+++ atomic-swaps/test.sol
@@ -0,0 +1,133 @@
+pragma solidity ^0.4.17;
+
+contract Token {
+  /// @return total amount of tokens
+  function totalSupply() constant returns (uint256 supply) {}
+
+  /// @param _owner The address from which the balance will be retrieved
+  /// @return The balance
+  function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+  /// @notice send `_value` token to `_to` from `msg.sender`
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return Whether the transfer was successful or not
+  function transfer(address _to, uint256 _value) returns (bool success) {}
+
+  /// @notice send `_value` token to `_to` from `_from` on the condition it is approved by `_from`
+  /// @param _from The address of the sender
+  /// @param _to The address of the recipient
+  /// @param _value The amount of token to be transferred
+  /// @return Whether the transfer was successful or not
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+  /// @notice `msg.sender` approves `_addr` to spend `_value` tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @param _value The amount of wei to be approved for transfer
+  /// @return Whether the approval was successful or not
+  function approve(address _spender, uint256 _value) returns (bool success) {}
+
+  /// @param _owner The address of the account owning tokens
+  /// @param _spender The address of the account able to transfer the tokens
+  /// @return Amount of remaining tokens allowed to spent
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+  event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+  uint public decimals;
+  string public name;
+}
+
+contract StandardToken is Token {
+    function StandardToken(uint initial) {
+        balances[msg.sender] = initial;
+    }
+
+  function transfer(address _to, uint256 _value) returns (bool success) {
+    //Default assumes totalSupply can't be over max (2^256 - 1).
+    //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+    //Replace the if with this one instead.
+    if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+    //if (balances[msg.sender] >= _value && _value > 0) {
+      balances[msg.sender] -= _value;
+      balances[_to] += _value;
+      Transfer(msg.sender, _to, _value);
+      return true;
+    } else { return false; }
+  }
+
+  function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+    //same as above. Replace this line with the following if you want to protect against wrapping uints.
+    if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+    //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+      balances[_to] += _value;
+      balances[_from] -= _value;
+      allowed[_from][msg.sender] -= _value;
+      Transfer(_from, _to, _value);
+      return true;
+    } else { return false; }
+  }
+
+  function balanceOf(address _owner) constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+// msg.sender the _spender to spend _value amount of tokens
+  function approve(address _spender, uint256 _value) returns (bool success) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+  // people who have coins
+  mapping(address => uint256) balances;
+
+  // people wo are allowed to spend the coins     
+  mapping (address => mapping (address => uint256)) allowed;
+
+  uint256 public totalSupply;
+}
+
+contract ERC20toERC20
+{
+    address token1ProviderAddr;
+    uint tokenOneAmount;
+      
+    address token2ProviderAddr;  
+    uint tokenTwoAmount;
+  
+    address ERC20contractOne;
+    address ERC20contractTwo;
+    
+    StandardToken stdTokenOne;
+    StandardToken stdTokenTwo;  
+     
+    function init(uint tokens1, uint tokens2, address _token1ProviderAddr, address _token2ProviderAddr, address contractAddress1, address contractAddress2) public{
+        ERC20contractOne  = contractAddress1;
+        stdTokenOne = StandardToken(ERC20contractOne);
+        
+        ERC20contractTwo  = contractAddress2;
+        stdTokenTwo = StandardToken(ERC20contractTwo);
+        
+        tokenOneAmount = tokens1;
+        tokenTwoAmount = tokens2;
+        
+        token1ProviderAddr = _token1ProviderAddr;
+        token2ProviderAddr = _token2ProviderAddr;
+    }
+    
+    //remove payable!!
+    function settle() public payable
+    {
+        require(stdTokenOne.allowance(token1ProviderAddr,address(this)) >= tokenOneAmount);
+        require(stdTokenTwo.allowance(token2ProviderAddr,address(this)) >= tokenTwoAmount);
+        stdTokenOne.transferFrom(token1ProviderAddr,  token2ProviderAddr, tokenOneAmount);
+        stdTokenTwo.transferFrom(token2ProviderAddr,  token1ProviderAddr, tokenTwoAmount);
+        
+    }
+}
